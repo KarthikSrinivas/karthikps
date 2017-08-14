77d13e4e07f0c9ca592249ff730174890d76ec88
KarthikSrinivasan <1416047@saec.ac.in>
2017-08-14 12:14:06 +0530

    Add files via upload

---
+++ Sa.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class Sa
+[
+public static void main(String rags[])
+{
+Sa s=new Sa();
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+inr a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+s.function(a,n);
+}
+public void function(int arr[],int n1)
+{
+for(int j=0;j<n;j++)
+{
+if(arr[j]==j)
+{
+int k=j;
+}
+}
+System.out.println(k);
+}
+}
